76453ed19c39ba26530b25eb8b3657bb9777dd14
Igor Storozhuk <istorozhuk@1cbit.ru>
2023-11-09 17:29:53 +0300

    Заменен устаревший флаг --rm-dist на --clean

--- goreleaser.sh
+++ goreleaser.sh
@@ -4,4 +4,4 @@ docker run --rm --privileged \
   -v $PWD:/go/src/app \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -w /go/src/app \
-  goreleaser/goreleaser:latest release --snapshot --skip-publish --rm-dist
+  goreleaser/goreleaser:latest release --snapshot --skip-publish --clean
